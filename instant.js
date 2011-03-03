d54e296d79945a8dd01c40c9bc3bfa77214a5d13
Ernesto Carrión <carrion.silver@gmail.com>
2011-03-03 16:20:53 -0500

    Added instant translate

--- instant.js
+++ instant.js
@@ -1,11 +1,25 @@
+//Shows a translating tooltip
 function show_translation(startX, startY, endX, endY, text){
     
-    $('body').append("<div class='quick-tooltip round'>" + text + "</div>");
+    $('body').append("<div id='qt' class='quick-tooltip round'> Translating... </div>");
     $('div.quick-tooltip').css('top', startY - 40);
     $('div.quick-tooltip').css('left', startX);
     
+    getTranslation(text);
 }
 
+function getTranslation(text_to_translate) {
+    
+    //Sends a request to the background_page to make the ajax translation
+    chrome.extension.sendRequest({text: text_to_translate}, function(response) {
+        $('#qt').text(response);
+    }); 
+}
+
+
+//Delete the tooltip
 function delete_translation(){
     $('div.quick-tooltip').remove();
 }
+
+
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Created by Ernesto Carrión
 
+//Redirects to the google translate page with the desired translation
 function translate(info, tab) {
 
   var url = "http://translate.google.com/?hl=es#en|es|" + info.selectionText;
@@ -10,6 +11,20 @@ function translate(info, tab) {
   
 }
 
+//Loads google api
+google.load("language", "1");
+
+//listens from messages from original pages
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+    
+        
+        google.language.translate(request.text, 'en', 'es', function(result) {
+            translation = result.translation.charAt(0).toUpperCase()
+                          + result.translation.substring(1, result.translation.length);
+            sendResponse(translation);
+        });
+});
 
 // Create the selection context item
 var context = ['selection'];
